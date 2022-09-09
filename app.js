f5221cc44bac9febfeff57dc4e3ccd6e78a3b3ab
Stefan Kondring <kondring.s@gmx.de>
2022-09-09 16:43:27 +0200

    fix navbar

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const aboutMeBtn = document.querySelector('[data-js="toggle-btn"]');
-const aboutMeText = document.querySelector('[data-js="toggle-text"]'); 
+const aboutMeBtn = document.querySelector('[data-js="toggle-btn-about"]');
+const aboutMeText = document.querySelector('[data-js="toggle-text-about"]');
+const contactBtn = document.querySelector('[data-js="toggle-btn-contact"]');
+const contactText = document.querySelector('[data-js="toggle-text-contact"]');  
+const projectsBtn = document.querySelector('[data-js="toggle-btn-projects"]');
+const projectsText = document.querySelector('[data-js="toggle-text-projects"]');  
+const navIcon = document.querySelector('[data-js="header__nav-icon"]');
 
 aboutMeBtn.addEventListener('click', () => {
     aboutMeText.classList.toggle("hidden");
 })
+contactBtn.addEventListener('click', () => {
+    contactText.classList.toggle("hidden");
+})
+projectsBtn.addEventListener('click', () => {
+    projectsText.classList.toggle("hidden");
+})
+
 
 const icon = document.querySelector('.icon');
+const verticalNav = document.querySelector('[data-js="vertical-nav"]');
 
 icon.addEventListener('click', (event) => {
  icon.classList.toggle("open");
-  });
+ verticalNav.classList.toggle("header__vertical-nav-hidden");
+});
