2eca53f7caeb4ddcfe79a95128cd871a164fc070
Stefan Kondring <kondring.s@gmx.de>
2022-09-09 09:07:18 +0200

    add menu icon

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const aboutMeText = document.querySelector('[data-js="toggle-text"]');
 aboutMeBtn.addEventListener('click', () => {
     aboutMeText.classList.toggle("hidden");
 })
+
+const icon = document.querySelector('.icon');
+
+icon.addEventListener('click', (event) => {
+ icon.classList.toggle("open");
+  });
